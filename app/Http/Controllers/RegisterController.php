8be11783486ef305242b410a2c1b1bd02025488b
hscomp2014 <hscomp2002@gmail.com>
2020-06-13 19:08:25 +0430

    end registration

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -59,6 +59,7 @@ class RegisterController extends Controller
         $sms->user_info = json_encode($user_info, JSON_UNESCAPED_UNICODE);
         Sms_validation::where('mobile', $sms->mobile)->delete();
         $sms->save();
+        $this->sendSmsCode($sms->mobile,$sms->sms_code);
         $smsMessage = "لطفا کد پیامک شده را وارد نمایید";
         return view('layouts.register',
             [
@@ -132,6 +133,7 @@ class RegisterController extends Controller
         $marketer->provinces_id = $userInfo->province;
         $marketer->city = $userInfo->city;
         $marketer->save();
+        Sms_validation::where('mobile',$userInfo->mobile)->delete();
         return view(
             'layouts.register',
             [
@@ -140,5 +142,17 @@ class RegisterController extends Controller
                 'final_step' => 1
             ]
         );
+    }
+    public function sendSmsCode($receptor,$token){
+        $api_key = "553133726A6962423652346246504B544C72766668784A6E384C61682B4D565349756B2B374D374C4A6D553D";
+        $url = "https://api.kavenegar.com/v1/$api_key/verify/lookup.json";
+        $ch = curl_init();
+        $url = $url."?receptor=$receptor&token=$token&template=aref";
+        curl_setopt($ch, CURLOPT_URL,$url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($result);
     }
 }
