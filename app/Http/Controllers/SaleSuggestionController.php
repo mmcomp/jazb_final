4d78be0317e09de79809010bbc590dea91cc54db
mirsamie <mirsamie@PROGRAMMER11>
2020-06-25 15:49:19 +0430

    sale suggestions

--- app/Http/Controllers/SaleSuggestionController.php
+++ app/Http/Controllers/SaleSuggestionController.php
@@ -6,8 +6,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 use App\SaleSuggestion;
-
-use Exception;
+use App\Source;
+use App\Product;
+use App\School;
+use App\Tag;
 
 class SaleSuggestionController extends Controller
 {
@@ -23,38 +25,80 @@ class SaleSuggestionController extends Controller
 
     public function create(Request $request)
     {
+        $products = Product::where('is_deleted', false)->get();
+        $sources = Source::where('is_deleted', false)->get();
+        $moralTags = Tag::where('type', 'moral')->where('is_deleted', false)->orderBy('name')->get();
+        $needTags = Tag::where('type', 'need')->where('is_deleted', false)->orderBy('name')->get();
+        $schools = School::where('is_deleted', false)->get();
         $saleSuggestion = new SaleSuggestion;
         if($request->getMethod()=='GET'){
             return view('sale_suggestions.create', [
-                "saleSuggestion"=>$saleSuggestion
+                "saleSuggestion"=>$saleSuggestion,
+                "products"=>$products,
+                "sources"=>$sources,
+                "moralTags"=>$moralTags,
+                "needTags"=>$needTags,
+                "schools"=>$schools
             ]);
         }
 
         $saleSuggestion->name = $request->input('name', '');
+        $saleSuggestion->if_products_id = $request->input('if_products_id');
+        $saleSuggestion->if_moral_tags_id = $request->input('if_moral_tags_id');
+        $saleSuggestion->if_need_tags_id = $request->input('if_need_tags_id');
+        $saleSuggestion->if_schools_id = $request->input('if_schools_id');
+        $saleSuggestion->if_last_year_grade = $request->input('if_last_year_grade');
+        $saleSuggestion->if_avarage = $request->input('if_avarage');
+        $saleSuggestion->if_sources_id = $request->input('if_sources_id');
+        $saleSuggestion->users_id = Auth::user()->id;
+        $saleSuggestion->then_product1_id = $request->input('then_product1_id');
+        $saleSuggestion->then_product2_id = $request->input('then_product2_id');
+        $saleSuggestion->then_product3_id = $request->input('then_product3_id');
         $saleSuggestion->save();
 
-        $request->session()->flash("msg_success", "شرط. با موفقیت افزوده شد.");
+        $request->session()->flash("msg_success", "شرط با موفقیت افزوده شد.");
         return redirect()->route('sale_suggestions');
     }
 
     public function edit(Request $request, $id)
-    {
+    {;
         $saleSuggestion = SaleSuggestion::where('id', $id)->where('is_deleted', false)->first();
         if($saleSuggestion==null){
-            $request->session()->flash("msg_error", "شرط. مورد نظر پیدا نشد!");
+            $request->session()->flash("msg_error", "شرط مورد نظر پیدا نشد!");
             return redirect()->route('sale_suggestions');
         }
 
+        $products = Product::where('is_deleted', false)->get();
+        $sources = Source::where('is_deleted', false)->get();
+        $moralTags = Tag::where('type', 'moral')->where('is_deleted', false)->orderBy('name')->get();
+        $needTags = Tag::where('type', 'need')->where('is_deleted', false)->orderBy('name')->get();
+        $schools = School::where('is_deleted', false)->get();
         if($request->getMethod()=='GET'){
             return view('sale_suggestions.create', [
-                "saleSuggestion"=>$saleSuggestion
+                "saleSuggestion"=>$saleSuggestion,
+                "products"=>$products,
+                "sources"=>$sources,
+                "moralTags"=>$moralTags,
+                "needTags"=>$needTags,
+                "schools"=>$schools
             ]);
         }
 
         $saleSuggestion->name = $request->input('name', '');
+        $saleSuggestion->if_products_id = $request->input('if_products_id');
+        $saleSuggestion->if_moral_tags_id = $request->input('if_moral_tags_id');
+        $saleSuggestion->if_need_tags_id = $request->input('if_need_tags_id');
+        $saleSuggestion->if_schools_id = $request->input('if_schools_id');
+        $saleSuggestion->if_last_year_grade = $request->input('if_last_year_grade');
+        $saleSuggestion->if_avarage = $request->input('if_avarage');
+        $saleSuggestion->if_sources_id = $request->input('if_sources_id');
+        $saleSuggestion->users_id = Auth::user()->id;
+        $saleSuggestion->then_product1_id = $request->input('then_product1_id');
+        $saleSuggestion->then_product2_id = $request->input('then_product2_id');
+        $saleSuggestion->then_product3_id = $request->input('then_product3_id');
         $saleSuggestion->save();
 
-        $request->session()->flash("msg_success", "شرط. با موفقیت ویرایش شد.");
+        $request->session()->flash("msg_success", "شرط با موفقیت ویرایش شد.");
         return redirect()->route('sale_suggestions');
     }
 
@@ -62,14 +106,14 @@ class SaleSuggestionController extends Controller
     {
         $saleSuggestion = SaleSuggestion::where('id', $id)->where('is_deleted', false)->first();
         if($saleSuggestion==null){
-            $request->session()->flash("msg_error", "شرط. مورد نظر پیدا نشد!");
+            $request->session()->flash("msg_error", "شرط مورد نظر پیدا نشد!");
             return redirect()->route('sale_suggestions');
         }
 
         $saleSuggestion->is_deleted = true;
         $saleSuggestion->save();
 
-        $request->session()->flash("msg_success", "شرط. با موفقیت حذف شد.");
+        $request->session()->flash("msg_success", "شرط با موفقیت حذف شد.");
         return redirect()->route('sale_suggestions');
     }
 }
--- resources/views/sale_suggestions/create.blade.php
+++ resources/views/sale_suggestions/create.blade.php
@@ -39,14 +39,159 @@
                     <div class="col">
                         <div class="form-group">
                             <label for="name">نام</label>
-                            @if (isset($school) && isset($school->id))
-                            <input type="text" class="form-control" id="name" name="name" placeholder="نام" value="{{ $school->name }}" />
+                            @if (isset($saleSuggestion) && isset($saleSuggestion->id))
+                            <input type="text" class="form-control" id="name" name="name" placeholder="نام" value="{{ $saleSuggestion->name }}" />
                             @else
                             <input type="text" class="form-control" id="name" name="name" placeholder="نام"  />
                             @endif
                         </div>
+
+                        <div class="form-group">
+                            <label for="if_moral_tags_id">برچسب اخلاقی</label>
+                            <select  id="if_moral_tags_id" name="if_moral_tags_id" class="form-control">
+                                <option value="0"></option>
+                                @foreach ($moralTags as $item)
+                                    @if (isset($saleSuggestion) && isset($saleSuggestion->id) && $saleSuggestion->if_moral_tags_id == $item->id)
+                                    <option value="{{ $item->id }}" selected>
+                                    @else
+                                    <option value="{{ $item->id }}" >
+                                    @endif
+                                    {{ $item->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="if_schools_id">مدرسه</label>
+                            <select  id="if_schools_id" name="if_schools_id" class="form-control">
+                                <option value="0"></option>
+                                @foreach ($schools as $item)
+                                    @if (isset($saleSuggestion) && isset($saleSuggestion->id) && $saleSuggestion->if_schools_id == $item->id)
+                                    <option value="{{ $item->id }}" selected>
+                                    @else
+                                    <option value="{{ $item->id }}" >
+                                    @endif
+                                    {{ $item->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="if_avarage">معدل</label>
+                            @if(isset($saleSuggestion) && isset($saleSuggestion->id))
+                            <input type="number" class="form-control" id="if_avarage" name="if_avarage" placeholder="معدل" value="{{ $saleSuggestion->if_avarage }}" />
+                            @else
+                            <input type="number" class="form-control" id="if_avarage" name="if_avarage" placeholder="معدل" />
+                            @endif
+                        </div>
+
+                        <div class="form-group">
+                            <label for="then_product1_id">محصول پیشنهادی 1</label>
+                            <select  id="then_product1_id" name="then_product1_id" class="form-control">
+                                <option value="0"></option>
+                                @foreach ($products as $item)
+                                    @if (isset($saleSuggestion) && isset($saleSuggestion->id) && $saleSuggestion->then_product1_id == $item->id)
+                                    <option value="{{ $item->id }}" selected>
+                                    @else
+                                    <option value="{{ $item->id }}" >
+                                    @endif
+                                    {{ $item->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="then_product3_id">محصول پیشنهادی 3</label>
+                            <select  id="then_product3_id" name="then_product3_id" class="form-control">
+                                <option value="0"></option>
+                                @foreach ($products as $item)
+                                    @if (isset($saleSuggestion) && isset($saleSuggestion->id) && $saleSuggestion->then_product3_id == $item->id)
+                                    <option value="{{ $item->id }}" selected>
+                                    @else
+                                    <option value="{{ $item->id }}" >
+                                    @endif
+                                    {{ $item->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
                     </div>
                     <div class="col">
+                        <div class="form-group">
+                            <label for="if_products_id">محصول</label>
+                            <select  id="if_products_id" name="if_products_id" class="form-control">
+                                <option value="0"></option>
+                                @foreach ($products as $item)
+                                    @if (isset($saleSuggestion) && isset($saleSuggestion->id) && $saleSuggestion->if_products_id == $item->id)
+                                    <option value="{{ $item->id }}" selected>
+                                    @else
+                                    <option value="{{ $item->id }}" >
+                                    @endif
+                                    {{ $item->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="if_need_tags_id">برچسب نیازسنجی</label>
+                            <select  id="if_need_tags_id" name="if_need_tags_id" class="form-control">
+                                <option value="0"></option>
+                                @foreach ($needTags as $item)
+                                    @if (isset($saleSuggestion) && isset($saleSuggestion->id) && $saleSuggestion->if_need_tags_id == $item->id)
+                                    <option value="{{ $item->id }}" selected>
+                                    @else
+                                    <option value="{{ $item->id }}" >
+                                    @endif
+                                    {{ $item->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="if_last_year_grade">رتبه</label>
+                            @if(isset($saleSuggestion) && isset($saleSuggestion->id))
+                            <input type="number" class="form-control" id="if_last_year_grade" name="if_last_year_grade" placeholder="رتبه" value="{{ $saleSuggestion->if_last_year_grade }}" />
+                            @else
+                            <input type="number" class="form-control" id="if_last_year_grade" name="if_last_year_grade" placeholder="رتبه" />
+                            @endif
+                        </div>
+
+                        <div class="form-group">
+                            <label for="if_sources_id">منبع</label>
+                            <select  id="if_sources_id" name="if_sources_id" class="form-control">
+                                <option value="0"></option>
+                                @foreach ($sources as $item)
+                                    @if (isset($saleSuggestion) && isset($saleSuggestion->id) && $saleSuggestion->if_sources_id == $item->id)
+                                    <option value="{{ $item->id }}" selected>
+                                    @else
+                                    <option value="{{ $item->id }}" >
+                                    @endif
+                                    {{ $item->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="then_product2_id">محصول پیشنهادی 2</label>
+                            <select  id="then_product2_id" name="then_product2_id" class="form-control">
+                                <option value="0"></option>
+                                @foreach ($products as $item)
+                                    @if (isset($saleSuggestion) && isset($saleSuggestion->id) && $saleSuggestion->then_product2_id == $item->id)
+                                    <option value="{{ $item->id }}" selected>
+                                    @else
+                                    <option value="{{ $item->id }}" >
+                                    @endif
+                                    {{ $item->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
                     </div>
                 </div>
                 <div class="row">
