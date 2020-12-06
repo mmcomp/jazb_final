f06bb318871cb67f5c62fca649e3850ba9b478b2
mirsamie <mirsamie@PROGRAMMER11>
2020-12-06 11:10:02 +0330

    .

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -82,15 +82,20 @@
                     </a>
                     <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
                         @foreach ($usermessages as $item)
-                        <a href="#" class="dropdown-item">
+                        <a href="{{ route('messages') }}" class="dropdown-item">
                             <div class="media">
+                                @if($item->themessage->user->image_path)
+                                <img src="/uploads/{{ $item->themessage->user->image_path }}" alt="User Avatar"
+                                    class="img-size-50 mr-3 img-circle">
+                                @else
                                 <img src="/dist/img/user1-128x128.jpg" alt="User Avatar"
                                     class="img-size-50 mr-3 img-circle">
+                                @endif
                                 <div class="media-body">
                                     <h3 class="dropdown-item-title">
                                         {{ $item->themessage->user->first_name }}
                                         {{ $item->themessage->user->last_name }}
-                                        <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
+                                        <!--<span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>-->
                                     </h3>
                                     <p class="text-sm">{{ $item->themessage->message }}</p>
                                     <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i>
@@ -99,84 +104,26 @@
                             </div>
                         </a>
                         <div class="dropdown-divider"></div>
+
                         @endforeach
-                        <!--
-          <a href="#" class="dropdown-item">
-            <div class="media">
-              <img src="/dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
-              <div class="media-body">
-                <h3 class="dropdown-item-title">
-                  Brad Diesel
-                  <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
-                </h3>
-                <p class="text-sm">Call me whenever you can...</p>
-                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-              </div>
-            </div>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <div class="media">
-              <img src="/dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-              <div class="media-body">
-                <h3 class="dropdown-item-title">
-                  John Pierce
-                  <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
-                </h3>
-                <p class="text-sm">I got your message bro</p>
-                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-              </div>
-            </div>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <div class="media">
-              <img src="/dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-              <div class="media-body">
-                <h3 class="dropdown-item-title">
-                  Nora Silvester
-                  <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
-                </h3>
-                <p class="text-sm">The subject goes here</p>
-                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-              </div>
-            </div>
-          </a>
-          <div class="dropdown-divider"></div>
-            -->
                         <a href="{{ route('messages') }}" class="dropdown-item dropdown-footer">صندوق پیام</a>
                     </div>
                 </li>
 
                 <!-- Notifications Dropdown Menu -->
-                <!--
-      <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="far fa-bell"></i>
-          <span class="badge badge-warning navbar-badge">15</span>
-        </a>
-        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-          <span class="dropdown-item dropdown-header">15 Notifications</span>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-envelope mr-2"></i> 4 new messages
-            <span class="float-right text-muted text-sm">3 mins</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-users mr-2"></i> 8 friend requests
-            <span class="float-right text-muted text-sm">12 hours</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-file mr-2"></i> 3 new reports
-            <span class="float-right text-muted text-sm">2 days</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-        </div>
-      </li>
-      -->
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link" data-toggle="dropdown" href="#">
+                    <i class="far fa-bell"></i>
+                    <span class="badge badge-warning navbar-badge">{{ count($usercircular) }}</span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                    <span class="dropdown-item dropdown-header">{{ count($usercircular) }} بخشنامه</span>
+                    <div class="dropdown-divider"></div>
+                    <a href="{{ route('circulars') }}" class="dropdown-item dropdown-footer">مشاهده همه</a>
+                    </div>
+                </li>
+
                 <!--
       <li class="nav-item">
         <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#">
@@ -202,7 +149,11 @@
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
+                        @if(Auth::user()->image_path)
+                        <img src="/uploads/{{ Auth::user()->image_path }}" class="img-circle elevation-2" alt="User Image">
+                        @else
                         <img src="/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                        @endif
                     </div>
                     <div class="info">
                         <a href="/login" class="d-block">
