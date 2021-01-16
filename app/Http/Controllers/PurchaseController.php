997ceb8ba3d217d0b146382538995af4d43b1bd3
Maryam <maryam.kh1374@gmail.com>
2021-01-16 14:11:05 +0330

    allStudents except archived and banned

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -36,7 +36,7 @@ class PurchaseController extends Controller
     public function create(Request $request)
     {
         $purchase = new Purchase();
-        $students = Student::where('is_deleted', false)->where('archived',true)->where('banned',true)->get();
+        $students = Student::where('is_deleted', false)->where('archived',false)->where('banned',false)->get();
         $products = Product::where('is_deleted', false)->with('collection')->orderBy('name')->get();
         foreach($products as $index => $product){
             $products[$index]->parents = "-";
