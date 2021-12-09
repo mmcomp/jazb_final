d96ba07da52b9861796913fa1fb93dc3947bf639
majid nazari <majidnazarister@gmail.com>
2021-12-09 15:02:56 +0330

    compact of all student crm

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -415,7 +415,7 @@
 <script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
 <!-- page script -->
 <script type="text/javascript">
-    let students = @JSON($students);
+    let students = @JSON($theStudents);
     let parentOnes = @JSON($parentOnes);
     let parentTwos = @JSON($parentTwos);
     let parentThrees = @JSON($parentThrees);
